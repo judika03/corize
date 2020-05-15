c3e59e4db5381259a33d11c14fc5182d9a8cf3a4
System Administrator <root@ID-AFRITAROSAVIANTI.local>
2020-05-15 10:16:14 +0700

    add

--- instance-single/outputs.tf
+++ instance-single/outputs.tf
@@ -2,3 +2,6 @@ output "instance" {
   description = "List of self-links for compute instances"
   value       = google_compute_instance.server.name
 }
+output "ip" {
+  value = google_compute_instance.server.network_interface.network
+}

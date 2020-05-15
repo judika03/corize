e746b48392ea0a26cd2c53c9205138e248bc1559
System Administrator <root@ID-AFRITAROSAVIANTI.local>
2020-05-15 12:09:32 +0700

    fd

--- firewall-rules/main.tf
+++ firewall-rules/main.tf
@@ -6,8 +6,8 @@ resource "google_compute_firewall" "fw-rule" {
 
   allow {
     protocol = var.protocol
-    ports    = var.ports
+    ports    = [var.ports]
   }
   target_tags   = [var.name]
-  source_ranges = var.source_ranges
+  source_ranges = [var.source_ranges]
 }

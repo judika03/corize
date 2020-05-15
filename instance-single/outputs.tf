086d41e2949a22ec8e1cca9e54973a39c32e9781
System Administrator <root@ID-AFRITAROSAVIANTI.local>
2020-05-15 10:08:34 +0700

    f

--- instance-single/outputs.tf
+++ instance-single/outputs.tf
@@ -1,4 +1,4 @@
 output "instance" {
   description = "List of self-links for compute instances"
-  value       = google_compute_instance.server.*.self_link
+  value       = google_compute_instance.server.name
 }

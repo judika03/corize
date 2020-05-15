8fb4cff241bc1245d609ec790b67ab9fbb16d9e6
System Administrator <root@ID-AFRITAROSAVIANTI.local>
2020-05-15 12:05:48 +0700

    f

--- firewall-rules/main.tf
+++ firewall-rules/main.tf
@@ -1,6 +1,7 @@
 resource "google_compute_firewall" "fw-rule" {
   name      = var.name
   network   = var.network
+  project   = var.project_id
   description = "Creates Firewall rule targetting tagged instances"
 
   allow {

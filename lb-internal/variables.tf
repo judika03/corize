7c770a238ef2e974683602fd7a80d2180e8bf909
System Administrator <root@ID-AFRITAROSAVIANTI.local>
2020-05-26 16:00:35 +0700

    lb

---
+++ lb-internal/variables.tf
@@ -0,0 +1,125 @@
+variable "project" {
+  description = "The project to deploy to, if not set the default provider project is used."
+}
+
+variable "region" {
+  description = "Region for cloud resources."
+  default     = "asia-southeast1"
+}
+
+variable "network" {
+  description = "Name of the network to create resources in."
+  default     = "default"
+}
+
+variable "subnetwork" {
+  description = "Name of the subnetwork to create resources in."
+  default     = "default"
+}
+
+variable "network_project" {
+  description = "Name of the project for the network. Useful for shared VPC. Default is var.project."
+  default     = ""
+}
+
+variable "name" {
+  description = "Name for the forwarding rule and prefix for supporting resources."
+}
+
+variable "backends" {
+  description = "List of backends, should be a map of key-value pairs for each backend, must have the 'group' key."
+  type        = list(object({ 
+    group = string}))
+     default = []
+}
+
+variable "session_affinity" {
+  description = "The session affinity for the backends example: NONE, CLIENT_IP. Default is `NONE`."
+  default     = "NONE"
+}
+
+variable "ports" {
+  description = "List of ports range to forward to backend services. Max is 5."
+  type        = list(string)
+
+}
+
+variable "health_check" {
+  description = "Health check to determine whether instances are responsive and able to do work"
+  type = object({
+    type                = string
+    check_interval_sec  = number
+    healthy_threshold   = number
+    timeout_sec         = number
+    unhealthy_threshold = number
+    response            = string
+    proxy_header        = string
+    port                = number
+    port_name           = string
+    request             = string
+    request_path        = string
+    host                = string
+  })
+    default = {
+    type                = "http"
+    check_interval_sec  = 1
+    healthy_threshold   = 4
+    timeout_sec         = 1
+    unhealthy_threshold = 5
+    response            = ""
+    proxy_header        = "NONE"
+    port                = 9200
+    port_name           = "health-elastic-port"
+    request             = ""
+    request_path        = "/"
+    host                = ""
+  }
+}
+
+
+variable "source_tags" {
+  description = "List of source tags for traffic between the internal load balancer."
+  type        = list(string)
+  default     = ["allow-lb-service"]
+}
+
+variable "target_tags" {
+  description = "List of target tags for traffic between the internal load balancer."
+  type        = list(string)
+  default    = ["allow-group1"]
+
+}
+
+variable "source_ip_ranges" {
+  description = "List of source ip ranges for traffic between the internal load balancer."
+  type        = list(string)
+  default     = null
+}
+
+variable "source_service_accounts" {
+  description = "List of source service accounts for traffic between the internal load balancer."
+  type        = list(string)
+  default     = null
+}
+
+variable "target_service_accounts" {
+  description = "List of target service accounts for traffic between the internal load balancer."
+  type        = list(string)
+  default     = null
+}
+
+variable "ip_address" {
+  description = "IP address of the internal load balancer, if empty one will be assigned. Default is empty."
+  default     = null
+}
+
+variable "ip_protocol" {
+  description = "The IP protocol for the backend and frontend forwarding rule. TCP or UDP."
+  default     = "TCP"
+}
+
+variable "service_label" {
+  description = "Service label is used to create internal DNS name"
+  default     = null
+  type        = string
+}

de5f4152bcce9573267cb091302bcd8c045e316b
System Administrator <root@ID-AFRITAROSAVIANTI.local>
2020-05-27 15:33:30 +0700

    d

--- elastic-search/service_account.tf
+++
@@ -1,8 +0,0 @@
-module "service_accounts" {
-  source        = "github.com/judika03/module-terraform/modules/service_account"
-  project_id    = var.project_id
-  prefix        = var.prefix
-  names         = ["elastic-test"]
-  project_roles = ["${var.project_id}=>roles/viewer","${var.project_id}=>roles/compute.admin",
-]
-}
